e0bb5eb461f50a46e87a2e66175efcde9e5c0e4b
Gojko Adzic <gojko@gojko.com>
2017-12-24 21:46:32 +0100

    workflow, book links

--- web/index.js
+++ web/index.js
@@ -1,29 +1,60 @@
 /*global require */
-const estimate = require('../src/estimate'),
+const queryString = require('query-string'),
+	estimate = require('../src/estimate'),
 	init = function () {
 		'use strict';
-		const form = document.getElementById('story-form'),
+		const queryData = queryString.parse(location.search),
+			form = document.getElementById('story-form'),
+			mainDiv = document.getElementById('main'),
+			bookLinksDiv = document.getElementById('books'),
+			newEstimateButton = document.getElementById('new-estimate'),
+			bookLinksButton = document.getElementById('book-links'),
 			resultDiv = document.getElementById('result'),
 			spinnerDiv = document.getElementById('spinner'),
+			actionsDiv = document.getElementById('actions'),
 			resultSpan = document.getElementById('result-text'),
+			inputFields = Array.from(form.querySelectorAll('input[type=text]')),
 			showSpinner = function () {
 				resultDiv.style.display = 'none';
 				spinnerDiv.style.display = 'block';
+				actionsDiv.style.display = 'none';
 			},
 			showResult = function () {
-				const text = Array.from(form.querySelectorAll('input[type=text]')).map(t => t.value).join(' ');
+				const text = inputFields.map(t => t.value).join(' ');
 				resultSpan.innerHTML = estimate(text);
 				spinnerDiv.style.display = 'none';
 				resultDiv.style.display = 'block';
+				actionsDiv.style.display = 'none';
+				newEstimateButton.focus();
+			},
+			initFields = function () {
+				inputFields.forEach(field => {
+					if (queryData[field.name]) {
+						field.value = queryData[field.name];
+					}
+				});
+			},
+			formFilled = function () {
+				return !inputFields.find(t => !t.value);
 			};
-		form.addEventListener('submit',	(event) => {
-			showSpinner();
-
-			setTimeout(showResult, Math.random() * 1500 + 500);
-			event.preventDefault();
+		newEstimateButton.addEventListener('click', e => {
+			actionsDiv.style.display = 'block';
+			spinnerDiv.style.display = 'none';
+			resultDiv.style.display = 'none';
+			inputFields[0].focus();
+			e.preventDefault();
 		});
-		document.querySelector('input[type=text]').focus();
+		bookLinksButton.addEventListener('click', e => {
+			bookLinksDiv.style.display = 'block';
+			mainDiv.style.display = 'none';
+			e.preventDefault();
+		});
+		initFields();
 		document.querySelector('body').classList.toggle('nojs');
+		if (formFilled()) {
+			showSpinner();
+			setTimeout(showResult, Math.random() * 1500 + 500);
+		}
 	};
 
 window.addEventListener('load', init);
